723ae8c070b404a4fc7356f5f3059409b6d7a298
psuman10 <psuman9840@gmail.com>
2022-01-19 11:11:33 +0545

    worked on login

--- car_rental/lib/screen/login.dart
+++ car_rental/lib/screen/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:motion_toast/motion_toast.dart';
+import '/http/httpuser.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -13,6 +14,11 @@ class _LoginScreenState extends State<LoginScreen> {
 
   String uname = '';
   String pass = '';
+
+  Future<bool> loginPost(String uname, String pass) {
+    var res = HttpConnectUser().loginPosts(uname, pass);
+    return res;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,82 +39,88 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Form(
               key: loginform,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   const Text(
-                    'LOGIN',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        height: 6.5,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  const Padding(
+                      'LOGIN',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          height: 6.5,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    const Padding(
                     padding: EdgeInsets.only(
                         top: 20.0, left: 120.0, right: 120.0, bottom: 12.0),
                     child: Divider(
                       thickness: 4,
                       color: Colors.red,
+                      
                     ),
                   ),
                   const SizedBox(height: 30),
-                  TextFormField(
-                    style: const TextStyle(
-                        color: Colors.white,
-                        backgroundColor: Colors.transparent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+                  TextFormField( style: const TextStyle(
+                          color: Colors.white,
+                          backgroundColor: Colors.transparent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                    
                     onSaved: (value) {
                       uname = value!;
                     },
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide:
-                            const BorderSide(color: Colors.white, width: 4.0),
-                      ),
-                      labelStyle: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(color: Colors.grey, width: 6.0)),
-                      fillColor: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 4.0),
+                          ),
+                          labelStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 6.0)),
+                       fillColor: Colors.white,
                       labelText: 'Username',
                       hintText: 'Enter Username',
+                      
                     ),
                   ),
                   const SizedBox(height: 30),
                   TextFormField(
-                    style: const TextStyle(
-                        color: Colors.white,
-                        backgroundColor: Colors.transparent,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+                     style: const TextStyle(
+                          color: Colors.white,
+                          backgroundColor: Colors.transparent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                    
                     onSaved: (newValue) {
                       pass = newValue!;
                     },
                     obscureText: true,
-                    decoration: InputDecoration(
+                    decoration:  InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide:
-                            const BorderSide(color: Colors.white, width: 4.0),
-                      ),
-                      labelStyle: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              const BorderSide(color: Colors.grey, width: 6.0)),
-                      fillColor: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 4.0),
+                          ),
+                          labelStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 6.0)),
+                       fillColor: Colors.white,
                       labelText: 'Password',
                       hintText: 'Enter Password',
+                   
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -116,7 +128,20 @@ class _LoginScreenState extends State<LoginScreen> {
                     style: ElevatedButton.styleFrom(
                       minimumSize: const Size(double.infinity, 50),
                     ),
-                    onPressed: () {},
+                    onPressed: () async {
+                      loginform.currentState!.save();
+                      var res = await loginPost(uname, pass);
+                      if (res) {
+                        Navigator.pushNamed(context, '/add_student');
+                        MotionToast.success(
+                          description: const Text('Login Successfull'),
+                          toastDuration: const Duration(seconds: 1),
+                        ).show(context);
+                      } else {
+                        MotionToast.error(description: const Text('Login UnSuccessfull'))
+                            .show(context);
+                      }
+                    },
                     child: const Text('Login'),
                   ),
                   const SizedBox(height: 30),
@@ -131,7 +156,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         style: TextStyle(
                           fontSize: 20,
                           color: Colors.blue,
-
+                          
                           // ,
                         ),
                       ),
