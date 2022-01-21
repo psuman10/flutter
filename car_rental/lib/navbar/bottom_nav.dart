1c1b490c02efea4fa70feae269368ef057e32bb8
psuman10 <psuman9840@gmail.com>
2022-01-21 10:34:16 +0545

    working on bottom navbar

---
+++ car_rental/lib/navbar/bottom_nav.dart
@@ -0,0 +1,60 @@
+import 'package:car_rental/screen/login.dart';
+import 'package:flutter/material.dart';
+class NavBottompract extends StatefulWidget {
+  const NavBottompract({Key? key}) : super(key: key);
+  @override
+  _NavBottompractState createState() => _NavBottompractState();
+}
+class _NavBottompractState extends State<NavBottompract> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOption = [
+    LoginScreen(),
+    LoginScreen(),
+    LoginScreen(),
+    LoginScreen()
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOption.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: 'Search',
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.compare_arrows,
+            ),
+            label: 'Bookings',
+          ),
+          BottomNavigationBarItem( 
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ), 
+        ],
+        currentIndex: _selectedIndex,
+        // selectedItemColor: Colors.amber[900],
+        // backgroundColor: Colors.teal,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
