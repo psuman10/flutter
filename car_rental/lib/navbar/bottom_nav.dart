6536148a39cbc5b8c58df81d9b43db0edd05128e
psuman10 <psuman9840@gmail.com>
2022-01-26 09:33:46 +0545

    working on main screen

--- car_rental/lib/navbar/bottom_nav.dart
+++ car_rental/lib/navbar/bottom_nav.dart
@@ -1,5 +1,6 @@
 import 'package:car_rental/profile_screen/body.dart';
 import 'package:car_rental/screen/login.dart';
+import 'package:car_rental/screen/main_screen.dart';
 import 'package:flutter/material.dart';
 class NavBottompract extends StatefulWidget {
   const NavBottompract({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class NavBottompract extends StatefulWidget {
 class _NavBottompractState extends State<NavBottompract> {
   int _selectedIndex = 0;
   static const List<Widget> _widgetOption = [
-    LoginScreen(),
+    Home(),
     LoginScreen(),
     LoginScreen(),
     Body(),
