5b233ec63e7cae386610d714afda81e7a06e8dd1
psuman10 <psuman9840@gmail.com>
2022-01-30 12:55:59 +0545

    working on user profile

---
+++ car_rental/lib/model/profile.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'profile.g.dart';
+
+@JsonSerializable()
+class Profile {
+  @JsonKey(name: '_id')
+  String? id;
+  String? fullname;
+  String? email;
+  String? gender;
+  String? address;
+  String? photo;
+
+  Profile(
+      {this.id,
+      this.fullname,
+      this.email,
+      this.gender,
+      this.address,
+      this.photo,});
+
+  factory Profile.fromJson(Map<String, dynamic> obj) => _$ProfileFromJson(obj);
+  Map<String, dynamic> toJson() => _$ProfileToJson(this);
+}
---
+++ car_rental/lib/model/profile.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Profile _$ProfileFromJson(Map<String, dynamic> json) => Profile(
+      id: json['_id'] as String?,
+      fullname: json['fullname'] as String?,
+      email: json['email'] as String?,
+      gender: json['gender'] as String?,
+      address: json['address'] as String?,
+      photo: json['photo'] as String?,
+    );
+
+Map<String, dynamic> _$ProfileToJson(Profile instance) => <String, dynamic>{
+      '_id': instance.id,
+      'fullname': instance.fullname,
+      'email': instance.email,
+      'gender': instance.gender,
+      'address': instance.address,
+      'photo': instance.photo,
+    };
