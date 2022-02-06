eb93fc90753843ac9216f7682f41dc72b5eb5492
psuman10 <psuman9840@gmail.com>
2022-02-06 17:38:08 +0545

    added car model

---
+++ car_rental/lib/model/addcar_model.dart
@@ -0,0 +1,10 @@
+
+  class Car {
+  final String? name;
+  final String? image;
+  final String? capacity;
+  final String? fuelType;
+  final String? rentPerHour;
+
+  Car({this.name, this.image, this.capacity, this.fuelType, this.rentPerHour});
+}
