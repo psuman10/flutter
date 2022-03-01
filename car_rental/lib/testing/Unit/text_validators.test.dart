7a920adc3f9d505bbb8ee95ee3e10e7f28d77757
psuman10 <psuman9840@gmail.com>
2022-03-01 08:27:00 +0545

    Doing Testing

---
+++ car_rental/lib/testing/Unit/text_validators.test.dart
@@ -0,0 +1,11 @@
+// import 'package:flutter_test/flutter_test.dart';
+// // import 'package:car_rental/testing/Unit/text_validators.test.dart';
+// import 'package:car_rental/screen/register.dart';
+
+// void main() {
+//   group('Test multiple email values', () {
+//     test('Single name is not an email', () {
+//       expect('test'.EmailValidator(), true);
+//     });
+//   });
+// }
